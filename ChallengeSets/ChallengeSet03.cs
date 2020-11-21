27042545e90cd97cdd3c5dc3fc967a19ecc9dfb1
ZodyT <Zoraida1988@yahoo.com>
2020-11-20 19:38:34 -0600

    LastMinusFirst

--- ChallengeSets/ChallengeSet03.cs
+++ ChallengeSets/ChallengeSet03.cs
@@ -79,7 +79,8 @@ namespace ChallengeSets
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return nums[nums.Length - 1] - nums[0];
         }
         public int[] GetOddsBelow100()
         {
