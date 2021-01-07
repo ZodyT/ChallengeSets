0897d9fda6ee69e5cbe5b3613dd651c0a92e371a
ZodyT <Zoraida1988@yahoo.com>
2021-01-06 19:52:37 -0600

    CollectionContainsWord

--- ChallengeSets/ChallengeSet06.cs
+++ ChallengeSets/ChallengeSet06.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengeSets
 {
@@ -7,28 +8,44 @@ namespace ChallengeSets
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            
+                if (words == null || words.Contains(null))
+                {
+                    return false;
+                }
+                if (ignoreCase)
+                {
+                    foreach (var w in words)
+                    {
+                        if (w.ToLower() == word.ToLower())
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return words.Contains(word);
+            
         }
 
         public bool IsPrimeNumber(int num)
         {
             //throw new NotImplementedException();
-             if (num == 2)
-             {
-                return true;
 
-             }
+                if (num == 2)
+                {
+                    return true;
+                }
+                else if (num <= 1)
+                {
+                    return false;
+                }
+                else
+                {
+                    return false;
+                }
+            
 
-            else if (num % 2 == 0 || num <= 1)
-            {
-                return false;
-            }
-          
-            else
-            {
-                return true;
-            }
-                
         }
 
         public int IndexOfLastUniqueLetter(string str)
